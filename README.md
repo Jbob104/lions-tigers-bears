ee51392cf924c91a87613f8f9caf2bd33a550368
Jacob <mysliwiec.jacob@gmail.com>
2026-09-19 23:31:24 -0500

    Update to Coding Standards

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lions, Tigers, Bears
+# Jacob Mysliwiec Lions, Tigers, and Bears, Oh My!
 This repository contains source code for a Lions, Tigers, and Bears application.
 
 Application designed by Brad Schiff and modified by Jacob Mysliwiec.
--- index.js
+++ index.js
@@ -71,7 +71,8 @@ function createSlideshow(images)
 
     function nextSlide()
     {
-        document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
+        document.getElementById("slideshow").insertAdjacentHTML("beforeend", 
+            `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
         deleteFirstPhotoDelay = setTimeout(function() 
         {
             document.querySelector(".slide").remove()
